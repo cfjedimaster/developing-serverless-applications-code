eb7577233eae5d7d6ae075c336dd6474441905b4
Raymond Camden <raymondcamden@gmail.com>
2017-07-25 15:48:31 -0500

    more work

---
+++ ch05/doubler2.js
@@ -0,0 +1,7 @@
+
+function main(args) {
+    let result = Number(args.number) * 2;
+
+    return { number:result };
+    
+}
---
+++ ch05/incrementor2.js
@@ -0,0 +1,7 @@
+
+function main(args) {
+    let result = Number(args.number) + 1;
+
+    return { number:result };
+    
+}
